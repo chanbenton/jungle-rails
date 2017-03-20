451bd77dca4d3c4f6137405185e6ee8be5c0337e
chanbenton <chan.benton+test@gmail.com>
2017-03-20 08:48:22 +0000

    HTTP Basic Authentication added

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,4 +1,5 @@
 class Admin::CategoriesController < ApplicationController
+  http_basic_authenticate_with :name => ENV['user'], :password => ENV['password']
 
   def index
     @categories = Category.order(:name).all
